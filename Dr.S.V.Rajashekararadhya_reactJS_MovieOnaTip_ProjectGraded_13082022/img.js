0c0666dd6569bdf4a6825c9b631eee21a2fc0a55
Dr S V Rajashekararadhya <93774343+DrSVR@users.noreply.github.com>
2022-08-13 20:04:15 +0530

    Add files via upload

---
+++ Dr.S.V.Rajashekararadhya_reactJS_MovieOnaTip_ProjectGraded_13082022/img.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from "C:\Users\ANUbrr\Documents\React_project_GL\movies-app\src\img.jpg"
+
+function Imagess()
+{
+    return(
+        <div>
+        <img src="C:\Users\ANUbrr\Documents\React_project_GL\movies-app\src\img.jpg"alt=""/>
+        </div>
+    )
+}
+export default Imagess;
